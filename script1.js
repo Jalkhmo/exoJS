0d9b47fe7cc63d2e2dfaf0dbec3a902a87e9b428
Jalkhmo <ponthieujade@gmail.com>
2025-03-19 09:26:17 +0100

    Duplication

--- script1.js
+++ script1.js
@@ -1,17 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {  
-    const copierBtn = document.getElementById("copierBtn");
-    const texteACopier = document.getElementById("texteACopier").textContent; //recupere le texte
-    const confirmationMessage = document.getElementById("confirmationMessage");
-  
-    copierBtn.addEventListener("click", () => {
-      navigator.clipboard.writeText(texteACopier) //API de copie de texte + fonction pour ecrire dans le pp
-        .then(() => {   //message de validation (ou non)
-          confirmationMessage.textContent = "✅ Texte copié dans le presse-papier !";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const blocs = document.querySelectorAll(".bloc");
+
+  blocs.forEach(bloc => {
+    const bouton = bloc.querySelector(".copierBtn");
+    const texte = bloc.querySelector(".texteACopier").textContent; //recupere le texte
+    const confirmation = bloc.querySelector(".confirmationMessage");
+
+    bouton.addEventListener("click", () => {
+      navigator.clipboard.writeText(texte)  //API de copie de texte + fonction pour ecrire dans le pp
+        .then(() => { //message de validation (ou non)
+          confirmation.textContent = "✅ Texte copié dans le presse-papier !";
+          confirmation.style.color = "green";
         })
         .catch(err => {
-          confirmationMessage.textContent = "❌ Une erreur s'est produite lors de la copie.";
+          confirmation.textContent = "❌ Une erreur s'est produite lors de la copie.";
+          confirmation.style.color = "red";
           console.error("Erreur de copie :", err);
         });
     });
   });
+});
+
   
